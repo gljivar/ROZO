3b0ff1fe26d8351e3e89921957043524fe557162
psyburn <psykill@gmail.com>
2012-09-02 18:02:16 +0200

    Logic for showing categories and questions on index page.

--- app/router.js
+++ app/router.js
@@ -1,9 +1,8 @@
 define([
-  // Application.
-  "app"
-],
+// Application.
+"app", "modules/category", "modules/question"],
 
-function(app) {
+function(app, Category, Question) {
 
   // Defining the application router, you can attach sub routers here.
   var Router = Backbone.Router.extend({
@@ -11,9 +10,52 @@ function(app) {
       "": "index"
     },
 
-    index: function() {
+    initialize: function() {
+      // Set up the users.
+      this.questions = new Question.Collection();
 
+      this.categories = new Category.Collection();
+
+      // Use main layout and set Views.
+      app.useLayout("main");
+
+      app.layout.setViews({
+        ".questions": new Question.Views.List({
+          collection: this.questions
+        }),
+        ".categories": new Category.Views.List({
+          collection: this.categories
+        })
+      });
+    },
+
+    index: function() {
+      app.useLayout("main").render();
+      //mock data
+      this.questions.add([{
+        description: 'Description one.'
+      }, {
+        description: 'Desription two.'
+      }]);
+
+      this.categories.add([{
+        name: 'Semestar 1',
+        id: 1,
+        can_add_questions: false,
+        parent: null
+      },{
+        name: 'Math',
+        id: 2,
+        can_add_questions: true,
+        parent: 1
+      }
+      ]);
+
+      this.questions.trigger('reset');
+      this.categories.trigger('reset');
     }
+
+
   });
 
   return Router;
